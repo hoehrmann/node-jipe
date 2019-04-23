36661962be217bd3ab78c390cab1f7ab2b250e3d
Bjoern Hoehrmann <bjoern@hoehrmann.de>
2019-04-23 21:59:36 +0200

    standard errors are a class now

--- src/jsonrpc.ts
+++ src/jsonrpc.ts
@@ -1,21 +1,23 @@
+export type JsonrpcResult = { [key: string]: any };
 
-export type JsonrpcResult = { [key: string]: any; };
-
-export type JsonrpcParams = { [key: string]: any; } | null;
+export type JsonrpcParams = { [key: string]: any } | null;
 
 export type JsonrpcIdentifier = number;
 
-export type JsonrpcResponse = (
-  JsonrpcResponseError | JsonrpcResponseResult
-);
+export type JsonrpcResponse =
+  | JsonrpcResponseError
+  | JsonrpcResponseResult;
 
-export type JsonrpcMessage = (
-  JsonrpcRequest | JsonrpcResponse | JsonrpcNotification
-);
+export type JsonrpcMessage =
+  | JsonrpcRequest
+  | JsonrpcResponse
+  | JsonrpcNotification;
 
-export type JsonrpcMessageType = (
-  'failure' | 'result' | 'request' | 'malformed' 
-);
+export type JsonrpcMessageType =
+  | 'failure'
+  | 'result'
+  | 'request'
+  | 'malformed';
 
 export interface JsonrpcError {
   code: number;
@@ -48,10 +50,10 @@ export interface JsonrpcResponseResult extends WithVersion {
   id: JsonrpcIdentifier;
 }
 
-export interface JsonrpcStdErrors {
-  PARSE_ERROR:      { code: -32700, message: 'Parse error' };
-  INVALID_REQUEST:  { code: -32600, message: 'Invalid request' };
-  METHOD_NOT_FOUND: { code: -32601, message: 'Method not found' };
-  INVALID_PARAMS:   { code: -32602, message: 'Invalid params' };
-  INTERNAL_ERROR:   { code: -32603, message: 'Internal error' };
+export class JsonrpcStdErrors {
+  PARSE_ERROR = { code: -32700, message: 'Parse error' };
+  INVALID_REQUEST = { code: -32600, message: 'Invalid request' };
+  METHOD_NOT_FOUND = { code: -32601, message: 'Method not found' };
+  INVALID_PARAMS = { code: -32602, message: 'Invalid params' };
+  INTERNAL_ERROR = { code: -32603, message: 'Internal error' };
 }
