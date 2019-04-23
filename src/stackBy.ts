012c607ccb8ea9975835c2881c43800793222e8d
Bjoern Hoehrmann <bjoern@hoehrmann.de>
2019-04-23 21:58:25 +0200

    stackBy return type

--- src/stackBy.ts
+++ src/stackBy.ts
@@ -1,23 +1,20 @@
 /**
  * Creates stacks of adjacent elements of an iterable based on a key
  * extractor function.
- * 
+ *
  * @param iterable Iterable with elements to be grouped.
  * @param by Key extraction function.
  * @yields Stacks of adjacent elements with identical keys.
  */
-export function* stackBy<K,V>(iterable: V[], by: (
-    currentValue: V,
-    currentIndex: number,
-    iterable: V[]
-  ) => K) {
-
+export function* stackBy<K, V>(
+  iterable: V[],
+  by: (currentValue: V, currentIndex: number, iterable: V[]) => K
+): IterableIterator<V[]> {
   let previous: K | null = null;
   let group: V[] = [];
   let currentIndex = 0;
 
   for (const element of iterable) {
-
     const key = by(element, currentIndex++, iterable);
 
     if (previous !== null && key !== previous) {
@@ -27,9 +24,7 @@ export function* stackBy<K,V>(iterable: V[], by: (
 
     group.push(element);
     previous = key;
-
   }
 
   yield group;
-
 }
